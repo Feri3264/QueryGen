19b3b090d57c138891eace15e0132cb68489dcd3
Farid <tavakoli127@gmail.com>
2025-08-11 13:35:49 -0700

    Implementing IPasswordServices in QueryGen.Infrastructure

---
+++ src/QueryGen.Infrastructure/Common/Services/PasswordServices.cs
@@ -0,0 +1,22 @@
+using System;
+using QueryGen.Application.Common.Auth;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace QueryGen.Infrastructure.Common.Services;
+
+public class PasswordServices : IPasswordServices
+{
+    public string HashPassword(string Password)
+    {
+        Byte[] originalBytes;
+        Byte[] encodedBytes;
+        MD5 md5;
+        //Instantiate MD5CryptoServiceProvider, get bytes for original password and compute hash (encoded password)   
+        md5 = new MD5CryptoServiceProvider();
+        originalBytes = ASCIIEncoding.Default.GetBytes(Password);
+        encodedBytes = md5.ComputeHash(originalBytes);
+        //Convert encoded bytes back to a 'readable' string   
+        return BitConverter.ToString(encodedBytes);
+    }
+}
